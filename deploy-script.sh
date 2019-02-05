64eea2b84e011e1faf9ed4020b6ac3239ab7c82b
BasmaBoulekhras <basmaboulekhras@gmail.com>
2019-02-05 16:52:53 -0500

    deployment script

--- deploy-script.sh
+++ deploy-script.sh
@@ -8,7 +8,7 @@ deploy() {
   if [ "$1" == "master" -o "$1" == "develop" ] 
   then
     gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
-    helm upgrade --install "$3" "$4" --set image.tag=$1.$2
+    helm upgrade --install "$3" "$4" --set image.tag="$1.$2"
   else
   
     exit 0
