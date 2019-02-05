8b40c3fedd347fee354de29a4a1022642a568190
BasmaBoulekhras <basmaboulekhras@gmail.com>
2019-02-05 14:14:39 -0500

    ssh-keygen command adding

--- run-builder.bash
+++ run-builder.bash
@@ -10,6 +10,20 @@ ZONE=${ZONE:-us-central1-b}
 
 gcloud config set compute/zone ${ZONE}
 
+KEYNAME=builder-key
+
+ssh-keygen -t rsa -N "" -f ${KEYNAME} -C ${USERNAME} || true
+chmod 400 ${KEYNAME}*
+
+cat > ssh-keys <<EOF
+${USERNAME}:$(cat ${KEYNAME}.pub)
+EOF
+
+gcloud compute instances add-metadata ${INSTANCE_NAME} --metadata block-project-ssh-keys=TRUE --metadata-from-file ssh-keys=ssh-keys
+
+#Check if the instance's metadata is updated
+gcloud compute instances describe ${INSTANCE_NAME}
+
 ##gcloud compute ssh --ssh-key-file=ssh-keys/public-ssh-keys.txt test -- ${COMMAND} --dry-run
 
-gcloud compute ssh test -- ${COMMAND} --dry-run
+gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- ${COMMAND} --dry-run
