b96865ba1c484ca99713ddd5ed49ab20c5f70c7a
Jesús Adolfo Parra Parra <jadolfo.parra@gmail.com>
2019-02-06 16:29:58 -0500

    testing deploy-script

--- deploy-script.sh
+++ deploy-script.sh
@@ -5,15 +5,14 @@ CLUSTER=adolfo-test
 
 deploy() {
 
-  if [ "$1" == "master" -o "$1" == "develop" ] 
+  if [[ "$1" == "master" || "$1" == "develop" || "$1" == "remote-builder-test" ]];
   then
-    gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
-    helm upgrade --install "$3" "$4" --set image.tag="$1.$2"
+    #gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
+    #helm upgrade --install "$3" "$4" --set image.tag="$1.$2"
+    echo "\$1: $1; \$2: $2; \$3: $3; \$4: $4"
   else
-  
     exit 0
-    
   fi   
-    
 }
-#
+
+deploy $1 $2 $3 $4
