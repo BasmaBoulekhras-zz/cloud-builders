941333950536a302f6a7a80684906b40b2b295b2
Jesús Adolfo Parra Parra <jadolfo.parra@gmail.com>
2019-02-06 16:54:57 -0500

    dry-run helm

--- deploy-script.sh
+++ deploy-script.sh
@@ -7,9 +7,8 @@ deploy() {
 
   if [[ "$1" == "master" || "$1" == "develop" || "$1" == "remote-builder-test" ]];
   then
-    #gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
-    #helm upgrade --install "$3" "$4" --set image.tag="$1.$2"
-    echo "\$1: $1; \$2: $2; \$3: $3; \$4: $4"
+    gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
+    helm upgrade --install "$3" "$4" --set image.tag="$1.$2" --dry-run
   else
     exit 0
   fi   
