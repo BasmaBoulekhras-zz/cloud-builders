8d33c308f92643da035fd96d2d8e85c43b7c7731
BasmaBoulekhras <basmaboulekhras@gmail.com>
2019-02-07 14:01:34 -0500

    REMOTE_WORKSPACE injection

--- run-builder.bash
+++ run-builder.bash
@@ -4,13 +4,13 @@
 [ -z "$COMMAND" ] && echo "Need to set COMMAND" && exit 1;
 
 USERNAME=${USERNAME:-basma_boulekhras}
+BUILD_ID=${BUILD}
 #REMOTE_WORKSPACE=${REMOTE_WORKSPACE:-/home/${USERNAME}/workspace/}
-#REMOTE_WORKSPACE=${REMOTE_WORKSPACE:/home/${USERNAME}/tmp/${BUILD_ID}-workspace/}
-REMOTE_WORKSPACE=${REMOTE_WORKSPACE:/home/${USERNAME}/tmp/}
+REMOTE_WORKSPACE=${REMOTE_WORKSPACE:/home/${USERNAME}/tmp/${BUILD_ID}-workspace/}
+#REMOTE_WORKSPACE=${REMOTE_WORKSPACE:/home/${USERNAME}/tmp/}
 INSTANCE_NAME=${INSTANCE_NAME:-test}
 ZONE=${ZONE:-us-central1-b}
 INSTANCE_ARGS=${INSTANCE_ARGS:---preemptible}
-BUILD_ID=${BUILD}
 
 gcloud config set compute/zone ${ZONE}
 
@@ -32,18 +32,18 @@ gcloud compute instances describe ${INSTANCE_NAME}
 
 #Remove any existing files or directories from the remote instance
 function cleanup {
-    #rm -rf ${REMOTE_WORKSPACE}
-    rm -rf ${REMOTE_WORKSPACE}/${BUILD_ID}-workspace/
+    rm -rf ${REMOTE_WORKSPACE}
+    #rm -rf ${REMOTE_WORKSPACE}/${BUILD_ID}-workspace/
 }
 
 #create the build worksapce
 #gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir /home/${USERNAME}/tmp/${BUILD_ID}-workspace" 
-gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir ${REMOTE_WORKSPACE}/${BUILD_ID}-workspace" 
+gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir ${REMOTE_WORKSPACE}" 
 
 #copy the Workspace to the remote instance
 #gcloud compute scp --compress --recurse ./ ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE} --ssh-key-file=${KEYNAME}
 #gcloud compute scp --compress --recurse ./ ${USERNAME}@${INSTANCE_NAME}:/home/${USERNAME}/tmp/${BUILD_ID}-workspace/ --ssh-key-file=${KEYNAME}
-gcloud compute scp --compress --recurse ./ ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE}/${BUILD_ID}-workspace/ --ssh-key-file=${KEYNAME}
+gcloud compute scp --compress --recurse ./ ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE} --ssh-key-file=${KEYNAME}
 
 #ssh connection to the remote instance
 gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- ${COMMAND} 
