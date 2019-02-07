792355ac46033ce2373dac9991a3418d1077f45e
BasmaBoulekhras <basmaboulekhras@gmail.com>
2019-02-07 14:52:17 -0500

    basic helm command

--- deploy-script.sh
+++ deploy-script.sh
@@ -8,7 +8,8 @@ deploy() {
   if [[ "$1" == "master" || "$1" == "develop" || "$1" == "remote-builder-test" ]];
   then
     gcloud container clusters get-credentials --zone ${ZONE} ${CLUSTER}
-    helm upgrade --install "$3" "$4" --set image.tag="$1.$2" --dry-run
+    #helm upgrade --install "$3" "$4" --set image.tag="$1.$2" --dry-run
+    helm list
   else
     exit 0
   fi   
