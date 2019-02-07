3cac181b46e5f30030c498d8e2774f1780496bee
BasmaBoulekhras <basmaboulekhras@gmail.com>
2019-02-07 13:36:31 -0500

    Update run-builder.bash

--- run-builder.bash
+++ run-builder.bash
@@ -38,7 +38,7 @@ function cleanup {
 
 #create the build worksapce
 #gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir /home/${USERNAME}/tmp/${BUILD_ID}-workspace" 
-gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir ${REMOTE_WORKSPACE}/${BUILD_ID}-workspace" 
+gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --command "mkdir ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE}/${BUILD_ID}-workspace" 
 
 #copy the Workspace to the remote instance
 #gcloud compute scp --compress --recurse ./ ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE} --ssh-key-file=${KEYNAME}
